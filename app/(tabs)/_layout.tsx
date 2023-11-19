c4512d2a5b9e916dc91ec22b91f92cc8577df230
vansh <vansh7uppal@gmail.com>
2023-11-19 17:56:42 +0530

    feat: minor changes in tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,42 @@
 import { Tabs } from "expo-router";
+import Search from "../../src/assets/icon/search.svg";
+import Profile from "../../src/assets/icon/profile.svg";
+import { colors } from "../../src/design/color";
+
 export default function Layout() {
   return (
     <Tabs>
       <Tabs.Screen
         name="search"
-        options={{ tabBarLabel: "search", headerShown: false }}
+        options={{
+          tabBarLabel: "Search",
+          tabBarLabelStyle: {
+            color: "grey",
+          },
+          headerShown: false,
+          tabBarIcon: () => <Search />,
+          // tabBarBackground: colors.secondary,
+          tabBarStyle: {
+            padding: 10,
+            backgroundColor: colors.secondary,
+          },
+        }}
+      ></Tabs.Screen>
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: "Profile",
+          tabBarLabelStyle: {
+            color: "grey",
+          },
+          headerShown: false,
+          tabBarIcon: () => <Profile />,
+          // tabBarBackground: colors.secondary,
+          tabBarStyle: {
+            padding: 10,
+            backgroundColor: colors.secondary,
+          },
+        }}
       ></Tabs.Screen>
     </Tabs>
   );
