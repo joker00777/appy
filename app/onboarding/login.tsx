ea0bbee83e8dc4f31b08b288052762e7b750ddcd
arpit <arpit.rao@kratikal.com>
2023-11-19 12:52:58 +0530

    merge

--- app/onboarding/login.tsx
+++ app/onboarding/login.tsx
@@ -10,6 +10,7 @@ import { LoginValuesType, ResponseType } from "../../types";
 import Input from "../../src/components/Input";
 import { LoginApi } from "../../api";
 import Top from "../../src/components/Top";
+import BottomBar from "../../src/components/bottom"
 import { colors } from "../../src/design/color";
 import Google from '../../src/assets/icon/google.svg'
 
@@ -52,11 +53,12 @@ export default function Login() {
   }, [router]);
 
   return (
-    <View style ={{backgroundColor: colors.secondary, height:"100%"}} >
+    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, backgroundColor: colors.secondary }}>
       <Top text="Login" navigateBack={navigateBack} textColor="white" />
-      <View style={{ marginTop: 50, marginBottom: 40, marginLeft:'5%' }}>
-        <Text style={{ color: "#91919F", textAlign: "left", fontSize:40, fontWeight:"200"}}>
-         What's your email and password?
+      <View style={{ marginTop: 50, marginBottom: 40, marginLeft: '5%' }}>
+        <Text style={{ color: '#91919F', textAlign: 'left', fontSize: 40, fontWeight: '200' }}>
+          What's your email and password?
         </Text>
       </View>
       <Formik
@@ -67,73 +69,75 @@ export default function Login() {
         validateOnChange
         innerRef={formikRef}
       >
-      <View>
-            <Input placeholder="Email" name="email" style={{ padding: 10 }} />
-            <Input
-              placeholder="Password"
-              name="password"
-              style={{ padding: 10 }}
-            />
-    
+        <View>
+          <Input placeholder="Email" name="email" style={{ padding: 10 }} />
+          <Input placeholder="Password" name="password" style={{ padding: 10 }} />
+
           <Button
             text="Login"
             style={{
-              // backgroundColor: "#7F3DFF",
               borderRadius: 15,
               marginTop: 50,
               marginLeft: 10,
               marginRight: 10,
               height: 56,
               width: 380,
-              shadowColor: "#52006A",
+              shadowColor: '#52006A',
               elevation: 4,
             }}
             type="primary"
           />
         </View>
       </Formik>
-      <View style={{ marginTop: 50,}}>
-        <Text style={{ color: "#91919F", textAlign: "center" }}>
+      <View style={{ marginTop: 50 }}>
+        <Text style={{ color: '#91919F', textAlign: 'center' }}>
           Don’t have an account yet?
         </Text>
       </View>
-    <View style={{ marginTop: 0 }} >
-      <TouchableOpacity
-        style={{
-          alignItems: "center",
-        }}
-        // onPress={()=>}
-      >
-        <View
+      <View style={{ marginTop: 0 }}>
+        <TouchableOpacity
           style={{
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: 'white',
-            borderRadius: 15,
-            marginTop: 50,
-            marginLeft: 10,
-            marginRight: 10,
-            height: 56,
-            width: 380,
-            shadowColor: "#52006A",
-            elevation: 4,
-            flexDirection: "row",
-            flexBasis: "auto",
+            alignItems: 'center',
           }}
+          // onPress={()=>}
         >
-          <Google style={{ paddingRight: 50 }} />
-          <Text
+          <View
             style={{
-              fontWeight: "bold",
-              fontSize: 15,
-              color: 'black',
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'white',
+              borderRadius: 15,
+              marginTop: 50,
+              marginLeft: 10,
+              marginRight: 10,
+              height: 56,
+              width: 380,
+              shadowColor: '#52006A',
+              elevation: 4,
+              flexDirection: 'row',
+              flexBasis: 'auto',
             }}
           >
-            Continue With Google
-          </Text>
-        </View>
-      </TouchableOpacity>
+            {/* Assuming Google is an SVG or image component */}
+            <Google style={{ paddingRight: 50 }} />
+            <Text
+              style={{
+                fontWeight: 'bold',
+                fontSize: 15,
+                color: 'black',
+              }}
+            >
+              Continue With Google
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
-    </View>
+
+    {/* BottomBar positioned at the bottom of the screen */}
+    {/* <View style={{ position: 'absolute', bottom: 0, left: 0, right: 0 }}>
+      <BottomBar />
+    </View> */}
+  </View>
   );
 }
